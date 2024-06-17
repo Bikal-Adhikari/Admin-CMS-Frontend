0cc0de2434e7093227d3c53e865213ec93e06130
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-06-17 13:25:06 +1000

    Registration page is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
 import { ToastContainer } from "react-toastify";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/user/Login";
+import Register from "./pages/user/Register";
 
 function App() {
   return (
     <div>
-      <ToastContainer />
+      <Routes>
+        {/* public routes  */}
+        <Route path="/" element={<Login />}></Route>
+
+        {/* private routes  */}
+        <Route path="admin/new" element={<Register />}></Route>
+      </Routes>
+      <ToastContainer position="top-center" />
     </div>
   );
 }
