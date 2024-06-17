ac241c7c0ac2e7359d11b825d59a57eab7d6fb0e
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-06-17 11:31:50 +1000

    Admin CMS project Initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  return <>Hello world</>;
+  return (
+    <div>
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
